81443574d2e0291a2824d15ac85acb444c7ebc26
Sergey Kiskin <sector1200@gmail.com>
2024-05-15 14:22:09 +0500

    Set expess.static

--- sockets-and-carousel-server/index.ts
+++ sockets-and-carousel-server/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { createServer } from "node:http";
 // import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
+import { join } from "node:path";
 import { Server } from "socket.io";
+import { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.use(express.static("assets"));
+
 const server = createServer(app);
 const io = new Server(server);
 
@@ -16,7 +22,7 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    io.emit('chat message', msg);
+    io.emit("chat message", msg);
   });
 });
 
