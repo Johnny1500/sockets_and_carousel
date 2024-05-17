8d2d112302732c8d82fd5d0f2ce0593ac6e33b6e
Sergey Kiskin <sector1200@gmail.com>
2024-05-17 14:42:43 +0500

    Home page test markup

--- sockets-and-carousel-client/src/Components/HomeChat.tsx
+++ sockets-and-carousel-client/src/Components/HomeChat.tsx
@@ -1,30 +1,33 @@
-import { useRef } from "react";
+// import { useRef } from "react";
 import UserMessage from "./UserMessage";
+import ChatInput from "./ChatInput";
 
+// Чат на главной странице
 export default function HomeChat(): JSX.Element {
-  const currentUserMessageRef = useRef<string | null>(null);
+  // const currentUserMessageRef = useRef<string>('');
+  
+  // function handleUserInputChange(e: React.FormEvent<HTMLInputElement>) {
+  //   const target = e.target as HTMLInputElement;
 
-  function handleUserInputChange(e: React.FormEvent<HTMLInputElement>) {
-    const target = e.target as HTMLInputElement;
-
-    currentUserMessageRef.current = target.value;
-  }
+  //   currentUserMessageRef.current = target.value;
+  // }
 
   return (
     <section className="home-chat-container">
-      <div className="sticky top-0 min-h-[56px] text-xl font-semibold p-4 bg-white">
+      <div className="sticky top-0 h-[56px] text-xl font-semibold p-4 bg-white">
         Чат с поддержкой
       </div>
-      <div className="m-4 flex flex-col items-end gap-2">
+      <div className="user-message-container">
         <UserMessage>
           Информативное сообщение с просьбой, в две строки
         </UserMessage>
-        <UserMessage>
+        <UserMessage selfMessage={false}>
           Информативное сообщение с просьбой, в две строки
         </UserMessage>
       </div>
-      <form
-        className="home-chat-input-container"
+      <ChatInput />
+      {/* <form
+        className="chat-input-container"
         onSubmit={async (e) => {
           e.preventDefault();
           console.log(
@@ -33,6 +36,7 @@ export default function HomeChat(): JSX.Element {
           );
         }}
       >
+        {currentUserMessageRef.current}
         <input
           className="flex-1 text-[#77828C] pl-4 text-sm"
           maxLength={100}
@@ -52,11 +56,13 @@ export default function HomeChat(): JSX.Element {
               fillRule="evenodd"
               clipRule="evenodd"
               d="M18.9486 6.31623C19.0739 5.94035 18.9655 5.52595 18.6721 5.25962C18.3788 4.9933 17.9559 4.92528 17.5938 5.08619L4.92082 10.7186C3.61718 11.298 3.78221 13.1986 5.16622 13.5446L7.2927 14.0762C7.9053 14.2293 8.55425 14.0842 9.04323 13.6847L14.4913 9.23313C14.6777 9.08083 14.9248 9.32696 14.7732 9.51395L10.7598 14.4648C10.2656 15.0744 10.1736 15.9167 10.5246 16.6187L11.899 19.3675C12.4967 20.5628 14.2411 20.4388 14.6637 19.1711L18.9486 6.31623Z"
-              fill="#77828C"
+              fill={`${
+                currentUserMessageRef.current?.length > 0 ? "blue" : "grey"
+              }`}
             />
           </svg>
         </button>
-      </form>
+      </form> */}
     </section>
   );
 }
--- sockets-and-carousel-client/src/Components/UserMessage.tsx
+++ sockets-and-carousel-client/src/Components/UserMessage.tsx
@@ -8,19 +8,21 @@ export default function UserMessage({
   selfMessage = true,
 }: Props): JSX.Element {
   return (
-    <div className="relative">
-      <div />
-      <div className="flex flex-row items-end">
-        <div
-          className="w-4 h-4"
-          style={{ backgroundColor: `${selfMessage ? "#B9D7FB" : "#E2EAF1"}` }}
-        ></div>
-        <div
-          className="user-message"
-          style={{ backgroundColor: `${selfMessage ? "#B9D7FB" : "#E2EAF1"}` }}
-        >
-          {children}
-        </div>
+    <div className="flex flex-row items-end">
+      <div
+        className="w-6 h-6 -mr-5 rounded-full z-20"
+        style={{ backgroundColor: `${selfMessage ? "#B9D7FB" : "#E2EAF1"}` }}
+      ></div>
+      <div className="w-6 h-6 -mr-3 rounded-full bg-[#f1f3f5] z-10"></div>
+      <div
+        className="w-3 h-3"
+        style={{ backgroundColor: `${selfMessage ? "#B9D7FB" : "#E2EAF1"}` }}
+      />
+      <div
+        className="user-message"
+        style={{ backgroundColor: `${selfMessage ? "#B9D7FB" : "#E2EAF1"}` }}
+      >
+        {children}
       </div>
     </div>
   );
